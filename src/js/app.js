8c4bf9f679555330fd8acb656b84af76d3303965
JhojanP <jhojan.ynga999gmail.com>
2023-01-06 14:00:32 -0500

    20230106

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,19 @@
-                         
+  function reveal(){
+    var revels=document.querySelectorAll(".reveal");  
+    revels.forEach((reveal)=>{
+        var windowheight= window.innerHeight;
+        var elementTop=reveal.getBoundingClientRect().top;
+        var elementVisible=100;
+        console.log(windowheight)
+        console.log(elementTop)
+        if(elementTop < windowheight-elementVisible){
+            reveal.classList.add("active");
+        }else{
+            reveal.classList.remove("active");
+        }
+    });
+}
+window.addEventListener("scroll",function(){
+    reveal();
+});
  
