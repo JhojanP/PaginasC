c676b1e9d9cb7d642b4d413ca565bd5c8805290f
JhojanP <jhojan.ynga999gmail.com>
2023-01-13 00:20:49 -0500

    cambios 20230113 styles de paginas

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
-  /*
-  function reveal(){
+   
+function reveal(){
     var revels=document.querySelectorAll(".reveal");  
     revels.forEach((reveal)=>{
         var windowheight= window.innerHeight;
@@ -7,16 +7,16 @@
         var elementVisible=100;
         console.log(windowheight)
         console.log(elementTop)
+        
         if(elementTop < windowheight-elementVisible){
-            reveal.classList.add("active");
+            //reveal.classList.add("active");
         }else{
-            reveal.classList.remove("active");
+            //reveal.classList.remove("active");
         }
     });
 }
 window.addEventListener("scroll",function(){
     reveal();
 });
- 
-*/
+  
  
