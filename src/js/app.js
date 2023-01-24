e3b1cd3b3054c0b7cc1961f574c9ebd364dc7528
JhojanP <jhojan.ynga999gmail.com>
2023-01-24 12:34:32 -0500

    cambios 20012023 css modificacion de estilos

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,9 @@
-   
 function reveal(){
     var revels=document.querySelectorAll(".reveal");  
     revels.forEach((reveal)=>{
         var windowheight= window.innerHeight;
         var elementTop=reveal.getBoundingClientRect().top;
-        var elementVisible=100;
-        console.log(windowheight)
-        console.log(elementTop)
-        
+        var elementVisible=100; 
         if(elementTop < windowheight-elementVisible){
              reveal.classList.add("active");
         }else{
@@ -15,12 +11,25 @@ function reveal(){
         }
     });
 }
-window.addEventListener("scroll",function(e){
-    /*let rs=document.querySelectorAll(".rows");   
-    rs.forEach((r)=>{ 
-        r.classList.add("animate__animated","animate__fadeInUp");
-    })*/
+window.addEventListener("scroll",function(e){ 
     reveal()
 });
   
- 
+window.onload=function(){  
+    let content=document.getElementById("cianSlider");
+    if(content!=null){
+        document.getElementById("cianSlider").addEventListener("click",function(e){
+            let widtCianmenu=document.querySelector(".cian-menu").getBoundingClientRect().height; 
+            if(widtCianmenu>0 && widtCianmenu<=330)   {
+                document.getElementById("IconMenu").click();
+            }    
+        });
+    }       
+     
+    document.getElementById("IconMenu").addEventListener("click",function(e){ 
+        document.getElementById("IconI").classList.toggle("fa-window-close");
+    });
+
+     
+}
+
